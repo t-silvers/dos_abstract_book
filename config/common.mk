443279d45b2b35f09d99a5639cfcf187b07cfc73
t-silvers <tsilvers@stanford.edu>
2023-12-04 12:35:15 +0100

    squash me

--- config/common.mk
+++ config/common.mk
@@ -1,20 +1,20 @@
 DIR					?= /Users/thomassilvers/dev/projects/booklet_maker
 
-CFG					:= $(DIR)/config
-OUT					:= $(DIR)/results
-SCRIPTS				:= $(DIR)/scripts
-VENV				:= $(DIR)/venv
+CFG := $(DIR)/config
+OUT := $(DIR)/results
+SCRIPTS := $(DIR)/scripts
+VENV := $(DIR)/venv
 
-BOOKDIR				:= $(OUT)/dos_booklet
+BOOKDIR := $(OUT)/dos_booklet
 
-MKDIR				:= mkdir -p
-PYTHON				?= /Users/thomassilvers/mambaforge/bin/python
-# PYTHON			?= /usr/bin/python
-RCLONE				?= /usr/local/bin/rclone
+MKDIR := mkdir -p
+PYTHON ?= /Users/thomassilvers/mambaforge/bin/python
+# PYTHON ?= /usr/bin/python
+RCLONE ?= /usr/local/bin/rclone
 RCLONE_REMOTE		?= mpg_gdrive
-RCP					:= rclone copyto
+RCP := rclone copyto
 
-HOST				:= $(RCLONE_REMOTE):DoS
+HOST := $(RCLONE_REMOTE):DoS
 # Note: Must use extension for rclone copyto to work
 AMENDED_ORIG		:= "$(HOST)/registration_responses.xlsx"
 COMPLETE_ORIG		:= "$(HOST)/DoS2023Registration (Responses).xlsx"
@@ -23,11 +23,11 @@ AMENDED_LOCAL		:= $(OUT)/amended_registration_responses.xlsx
 COMPLETE_LOCAL		:= $(OUT)/raw_registration_responses.xlsx
 PUBLIC_RESPONSES	:= $(OUT)/registration_responses.csv
 BOOK_POSTERS		:= $(BOOKDIR)/posters.tex
-BOOK_TALKS			:= $(BOOKDIR)/talks.tex
+BOOK_TALKS := $(BOOKDIR)/talks.tex
 
-SMOKE_TEST          ?= 0
+SMOKE_TEST ?= 0
 export SMOKE_TEST
 
-.SHELLFLAGS			:= -eu -o pipefail -c
+.SHELLFLAGS := -eu -o pipefail -c
 .ONESHELL:
 .DELETE_ON_ERROR:
